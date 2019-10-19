e5b7aee783e889bde67bce34102f7936d8e27acc
Luca Lutz <iyfcjm@2mailcloud.com>
2019-10-19 11:41:04 +0200

    test1234

---
+++ Web/connect_db.php
@@ -0,0 +1,18 @@
+<?php
+    function OpenCon()
+     {
+     $dbhost = "localhost";
+     $dbuser = "dyingearth";
+     $dbpass = "raspberry";
+     $db = "dyingearth";
+     $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Verbindung fehlgeschlagen: %s\n". $conn -> error);
+     
+     return $conn;
+     }
+     
+    function CloseCon($conn)
+     {
+     $conn -> close();
+     }
+       
+    ?>
--- Web/index.php
+++ Web/index.php
@@ -1,6 +1,8 @@
 <?php
-
-
+include 'db_connection.php';
+$conn = OpenCon();
+echo "Connected Successfully";
+CloseCon($conn);
 ?>
 
 <html>
