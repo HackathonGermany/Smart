92a59035ed4ef8920e5ff5d719a3d254d597a057
ColdFireHunter <53087752+ColdFireHunter@users.noreply.github.com>
2019-10-19 14:23:00 +0200

    Database MySql Test
    ESP32

---
+++ Hardware/Hackathon_Database/src/main.cpp
@@ -0,0 +1,84 @@
+#include <Arduino.h>
+#ifdef ESP32
+  #include <WiFi.h>
+  #include <HTTPClient.h>
+#else
+  #include <ESP8266WiFi.h>
+  #include <ESP8266HTTPClient.h>
+  #include <WiFiClient.h>
+#endif
+
+#include <Wire.h>
+// Replace with your network credentials
+const char* ssid     = "EULE-Gast";
+const char* password = "@EULE_Zukunft!";
+
+// REPLACE with your Domain name and URL path or IP address with path
+const char* serverName = "http://192.168.1.179/API/post-esp-data.php";
+
+// Keep this API Key value to be compatible with the PHP code provided in the project page. 
+// If you change the apiKeyValue value, the PHP file /post-esp-data.php also needs to have the same key 
+String apiKeyValue = "tPmAT5Ab3j7F9";
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+
+void setup() {
+  Serial.begin(115200);
+  
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) { 
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  // (you can also pass in a Wire library object like &Wire2)
+ 
+}
+
+void loop() {
+  //Check WiFi connection status
+  if(WiFi.status()== WL_CONNECTED){
+    HTTPClient http;
+    
+    // Your Domain name with URL path or IP address with path
+    http.begin(serverName);
+    
+    // Specify content-type header
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    
+
+    String httpRequestData = "api_key=tPmAT5Ab3j7F9&strom=1&spannung=2&watt=3&lichtstaerke=4&temperatur=5&luftfeuchtigkeit=6&status=7&time=8+";             
+    Serial.print("httpRequestData: ");
+    Serial.println(httpRequestData);
+    
+    // You can comment the httpRequestData variable above
+    // then, use the httpRequestData variable below (for testing purposes without the BME280 sensor)
+    
+
+    // Send HTTP POST request
+    int httpResponseCode = http.POST(httpRequestData);
+     
+
+        
+    if (httpResponseCode>0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+    }
+    else {
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+    }
+    // Free resources
+    http.end();
+  }
+  else {
+    Serial.println("WiFi Disconnected");
+  }
+  //Send an HTTP POST request every 30 seconds
+  delay(30000);  
+}
