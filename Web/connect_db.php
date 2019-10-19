b5f4b50b65ff90c1ddba4610daf0c9efaa1e18a8
Luca Lutz <iyfcjm@2mailcloud.com>
2019-10-19 12:09:06 +0200

    test1234

--- Web/connect_db.php
+++ Web/connect_db.php
@@ -1,12 +1,12 @@
 <?php
     function OpenCon()
      {
-     $dbhost = "localhost";
+     $dbhost = "192.168.1.179";
      $dbuser = "dyingearth";
      $dbpass = "raspberry";
      $db = "dyingearth";
      $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Verbindung fehlgeschlagen: %s\n". $conn -> error);
-     
+     $pdo = new PDO('mysql:host="'.$dbhost.'";dbname="'.$db.'", "'.$dbuser.'", ""');
      return $conn;
      }
      
