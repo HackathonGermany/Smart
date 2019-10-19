f9c3fb610b85b369671856670594acd7be59e6ff
Luca Lutz <iyfcjm@2mailcloud.com>
2019-10-19 19:09:32 +0200

    luca

--- Web/Login/chart.php
+++ Web/Login/chart.php
@@ -39,9 +39,9 @@ BODY {
                     var temperatur = [];
 
                     for (var i in data) {
-                        test = "test";
+                        //test = "test";
                         //test = new Date(data[i].time * 1000).format('h:i:s');
-                        time.push(test);                        
+                        time.push(data[i].datum);                        
                         temperatur.push(data[i].temperatur);
                     }
 
