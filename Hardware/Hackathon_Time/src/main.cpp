a1769fe054cc3b704efebc48b1a6c05fd7842fc4
ColdFireHunter <53087752+ColdFireHunter@users.noreply.github.com>
2019-10-19 14:05:38 +0200

    Time NTP UNIX Test
    ESP 32

---
+++ Hardware/Hackathon_Time/src/main.cpp
@@ -0,0 +1,50 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include "time.h"
+
+const char* ssid       = "EULE-Gast";
+const char* password   = "@EULE_Zukunft!";
+
+const char* ntpServer = "0.de.pool.ntp.org";
+const long  gmtOffset_sec = 3600;
+const int   daylightOffset_sec = 3600;
+
+void printLocalTime()
+{
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+    }
+time_t now;
+time(&now);
+Serial.println(now);
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  
+  //connect to WiFi
+  Serial.printf("Connecting to %s ", ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+  }
+  Serial.println(" CONNECTED");
+  
+  //init and get the time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  printLocalTime();
+
+  //disconnect WiFi as it's no longer needed
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
+}
+
+void loop()
+{
+  delay(1000);
+  printLocalTime();
+}
