5154f211ce7d133a4fbbf48d00f1e89466471400
Luca Lutz <iyfcjm@2mailcloud.com>
2019-10-19 13:04:56 +0200

    luca

--- Web/API/post-esp-data-put.php
+++ Web/API/post-esp-data-put.php
@@ -7,7 +7,7 @@ $password = "esp";
 // Keep this API Key value to be compatible with the ESP32 code provided in the project page. 
 // If you change this value, the ESP32 sketch needs to match
 $api_key_value = "tPmAT5Ab3j7F9";
-
+echo $api_key_value;
 $api_key= $sensor = $location = $value1 = $value2 = $value3 = "";
 
 //if ($_SERVER["REQUEST_METHOD"] == "PUT") {
