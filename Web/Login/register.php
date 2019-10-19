3e6d267c405fee43918030b1e07b14f7996e6fbe
Luca Lutz <iyfcjm@2mailcloud.com>
2019-10-19 13:56:08 +0200

    luca

--- Web/Login/register.php
+++ Web/Login/register.php
@@ -50,7 +50,7 @@ if(isset($_GET['register'])) {
         $passwort_hash = password_hash($passwort, PASSWORD_DEFAULT);
         
         $statement = $pdo->prepare("INSERT INTO users (email, passwort, vorname, nachname) VALUES (:email, :passwort, :vorname, :nachname)");
-        $result = $statement->execute(array('email' => $email, 'passwort' => $passwort_hash));
+        $result = $statement->execute(array('email' => $email, 'passwort' => $passwort_hash, 'vorname' => $vorname, 'nachname' => $nachname));
         
         if($result) {        
             echo 'Du wurdest erfolgreich registriert. <a href="login.php">Zum Login</a>';
