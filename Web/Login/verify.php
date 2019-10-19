806fceb9cad686f8c761080abbb68e9e4da8de80
Maris <maris.beer@icloud.com>
2019-10-19 17:19:56 +0200

    updated verify

--- Web/Login/verify.php
+++ Web/Login/verify.php
@@ -1,9 +1,26 @@
 <?php
-$hash    = $_GET["hash"];
-$vorname = $_GET["vorname"];
 
-if(!isset($hash) && !isset($vorname)) 
+$pdo = new PDO('mysql:host=localhost;dbname=dyingearth', 'esp', 'esp');
+
+$unsafehash    = $_GET["hash"];
+$unsafevorname = $_GET["vorname"];
+
+if(!isset($unsafehash) || !isset($unsafevorname)) 
 {
     header("Location: /");
 }
+
+$hash = mysqli_real_escape_string($pdo, $unsafehash);
+$vorname = mysqli_real_escape_string($pdo, $unsafevorname);
+
+$statement = $pdo->prepare("SELECT * FROM 'users' WHERE vorname = :vorname");
+$result = $statement->execute(array('vorname' => $vorname));
+$user = $statement->fetch();
+if($user['passwort'] == $unsafehash)
+{
+    echo "verified!";
+} else {
+    echo "Hash or given name is invalid!";
+}
+
 ?>
