73e25ad91b6cee76f8067999f2af3687be4033e1
Luca Lutz <iyfcjm@2mailcloud.com>
2019-10-19 17:34:12 +0200

    luca

--- Web/Login/chart.php
+++ Web/Login/chart.php
@@ -63,6 +63,18 @@ BODY {
                     var barGraph = new Chart(graphTarget, {
                         type: 'line',
                         data: chartdata
+
+                        options: {
+                            layout: {
+                                padding: {
+                                    left: 50,
+                                    right: 0,
+                                    top: 0,
+                                    bottom: 0
+                                }
+                            }
+                        }
+
                     });
                 });
             }
