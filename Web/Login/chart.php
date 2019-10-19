7f7bb39ee652043effa27cf8b27cd48e4327c323
Luca Lutz <iyfcjm@2mailcloud.com>
2019-10-19 17:21:36 +0200

    luca

--- Web/Login/chart.php
+++ Web/Login/chart.php
@@ -1,23 +1,72 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>ChartJS - LineGraph</title>
-    <style>
-      .chart-container {
-        width: 640px;
-        height: auto;
-      }
-    </style>
-  </head>
-  <body>
-  test
-    <div class="chart-container">
-      <canvas id="mycanvas"></canvas>
+<head>
+<title>Creating Dynamic Data Graph using PHP and Chart.js</title>
+<style type="text/css">
+BODY {
+    width: 550PX;
+}
+
+#chart-container {
+    width: 100%;
+    height: auto;
+}
+</style>
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/Chart.min.js"></script>
+
+
+</head>
+<body>
+    <div id="chart-container">
+        <canvas id="graphCanvas"></canvas>
     </div>
-    
-    <!-- javascript -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script type="text/javascript" src="../js/Chart.min.js"></script>
-    <script type="text/javascript" src="chart.js"></script>
-  </body>
+
+    <script>
+        $(document).ready(function () {
+            showGraph();
+        });
+
+
+        function showGraph()
+        {
+            {
+                $.post("data.php",
+                function (data)
+                {
+                    console.log(data);
+                     var time = [];
+                    var temperatur = [];
+
+                    for (var i in data) {
+                        time.push(data[i].student_time);
+                        temperatur.push(data[i].temperatur);
+                    }
+
+                    var chartdata = {
+                        labels: time,
+                        datasets: [
+                            {
+                                label: 'Student temperatur',
+                                backgroundColor: '#49e2ff',
+                                borderColor: '#46d5f1',
+                                hoverBackgroundColor: '#CCCCCC',
+                                hoverBorderColor: '#666666',
+                                data: temperatur
+                            }
+                        ]
+                    };
+
+                    var graphTarget = $("#graphCanvas");
+
+                    var barGraph = new Chart(graphTarget, {
+                        type: 'bar',
+                        data: chartdata
+                    });
+                });
+            }
+        }
+        </script>
+
+</body>
 </html>
