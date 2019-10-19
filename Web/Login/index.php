f30602355f160882f8c10fbc50d2d0205195ed21
Luca Lutz <iyfcjm@2mailcloud.com>
2019-10-20 01:14:50 +0200

    luca

--- Web/Login/index.php
+++ Web/Login/index.php
@@ -49,6 +49,12 @@ foreach ($result as $row) {
         <link rel="stylesheet" href="https://stsackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="icon" type="image/vnd.microsoft.icon" href="assets/media/logo.ico">
+        <style>
+        th, td {
+          padding: 15px;
+          text-align: left;
+        }
+        </style>
         <!-- <meta http-equiv="refresh" content="1; URL=/Login/"> -->
     </head>
 <body>
