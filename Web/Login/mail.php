7125daf9a5098e4f51d2a51837dc5823d0737be5
Luca Lutz <iyfcjm@2mailcloud.com>
2019-10-19 15:26:05 +0200

    luca

---
+++ Web/Login/mail.php
@@ -0,0 +1,34 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'assets/PHPMailer/src/Exception.php';
+require 'assets/PHPMailer/src/PHPMailer.php';
+require 'assets/PHPMailer/src/SMTP.php';
+
+function sendVermail($email, $username, $verlink) {
+
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->SMTPDebug = 0; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
+$mail->Host = "smtp.gmail.com"; // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
+$mail->Port = 587; // TLS only
+$mail->SMTPSecure = 'tls'; // ssl is depracated
+$mail->SMTPAuth = true;
+$mail->Username = "example@gmail.com";
+$mail->Password = "psw";
+$mail->setFrom("example@gmail.com", "DyingEarth");
+$mail->addAddress($email, $username);
+$mail->Subject = 'Welcome to DyingEarth';
+$mail->msgHTML("<a href='$verlink'>-- Click here --</a>"); //$mail->msgHTML(file_get_contents('contents.html'), __DIR__); //Read an HTML message body from an external file, convert referenced images to embedded,
+$mail->AltBody = 'HTML messaging not supported';
+// $mail->addAttachment('images/phpmailer_mini.png'); //Attach an image file
+
+if(!$mail->send()){
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+}
+}
+?>
