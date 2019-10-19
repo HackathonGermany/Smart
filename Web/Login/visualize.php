c5825495ff8e1af89032d244753bf7e3ac370d65
Luca Lutz <iyfcjm@2mailcloud.com>
2019-10-19 16:51:59 +0200

    luca

--- Web/Login/visualize.php
+++ Web/Login/visualize.php
@@ -18,7 +18,7 @@ $result = mysqli_query($connect, $query);
 $rows = array();
 $table = array();
 
-$table['cols'] = array(
+$table['data'] = array(
  array(
   'label' => 'Date Time', 
   'type' => 'datetime'
@@ -43,7 +43,7 @@ while($row = mysqli_fetch_array($result))
      "c" => $sub_array
     );
 }
-$table['rows'] = $rows;
+$table['data'] = $rows;
 $jsonTable = json_encode($table);
 
 ?>
