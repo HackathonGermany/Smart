8eb62d2ee009ea1e48927e568085f0fc90d444e1
Jakob Weiss <jakob.l.weiss@gmx.de>
2019-10-19 12:56:39 +0200

    finished relay_script, fixed bugs

--- Hardware/RelayControl/src/main.cpp
+++ Hardware/RelayControl/src/main.cpp
@@ -1,11 +1,11 @@
 #include <Arduino.h>
 
-int relays[] = {2,3,4,5,6,7,8,9};
+int relays[] = {3,4,5,6,7,8,9};
 int relay_len = 7;
 void setup() {
   // put your setup code here, to run once:
   for (byte i = 0; i<relay_len;i++){
-    pinMode(i,OUTPUT);
+    pinMode(relays[i],OUTPUT);
   }
 }
 
@@ -20,7 +20,7 @@ void loop() {
     delay(200);
   }
   delay(1000);
-  for (int i = 0; i < relay_len;i++){
+  for (int i = relay_len-1; i >= 0;i--){
     relay(i,false);
     delay(200);
   }
