922ac31279bc799effd180c0cb19a29f861efb54
Luca Lutz <iyfcjm@2mailcloud.com>
2019-10-19 15:44:18 +0200

    luca

--- Web/Login/mail.php
+++ Web/Login/mail.php
@@ -3,9 +3,9 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '../assets/PHPMailer/src/Exception.php';
-require '../assets/PHPMailer/src/PHPMailer.php';
-require '../assets/PHPMailer/src/SMTP.php';
+require '../assets/vendor/PHPMailer/src/Exception.php';
+require '../assets/vendor/PHPMailer/src/PHPMailer.php';
+require '../assets/vendor/PHPMailer/src/SMTP.php';
 
 function sendVermail($email, $username, $verlink) {
 
