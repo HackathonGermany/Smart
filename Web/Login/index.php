ad3f24c1ab1aef0f57cf87ba63bcd0b78ec1c173
Maris <maris.beer@icloud.com>
2019-10-20 10:52:54 +0200

    modal try

--- Web/Login/index.php
+++ Web/Login/index.php
@@ -71,7 +71,10 @@ foreach ($result as $row) {
 <?php
 //echo "Hallo Benutzer mit der ID: ".$userid;
 ?>
-<button data-toggle="modal" data-target="#myModal">Click</button>
+
+<!-- Trigger the modal with a button -->
+<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+
 <!-- Modal -->
 <div id="myModal" class="modal fade" role="dialog">
   <div class="modal-dialog">
@@ -80,7 +83,7 @@ foreach ($result as $row) {
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Werte</h4>
+        <h4 class="modal-title">Modal Header</h4>
       </div>
       <div class="modal-body">
         <p>Some text in the modal.</p>
