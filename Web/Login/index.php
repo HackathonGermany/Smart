89f9439a17fa645932c342821d67d7e3019bd255
Maris <maris.beer@icloud.com>
2019-10-20 13:48:43 +0200

    dsdsds

--- Web/Login/index.php
+++ Web/Login/index.php
@@ -124,7 +124,6 @@ foreach ($result as $row) {
       <td><a data-target="#lichtstaerkeModal" data-toggle="modal" href="#lichtstaerkeModal"><?php print_r($data["0"]["lichtstaerke"]); ?></a></td>
       <td>//</td>
       <td class="col-sm-3">LED-Band</td>
-      <td></td>
       <td><input type="radio" name="relais1" value="102">Yellow</td>
      </tr>
      <tr>
@@ -146,6 +145,7 @@ foreach ($result as $row) {
       <td><a data-target="#statusModal" data-toggle="modal" href="#statusodal"><?php print_r($data["0"]["status"]); ?></a></td>
       <td>//</td>
       <td></td>
+      <form formmethod="get" action="index.php">
       <td><input type="radio" name="relais1" value="206">White</td>
      </tr>
      <tr>
@@ -170,44 +170,17 @@ foreach ($result as $row) {
       <td><input type="radio" name="relais1" value="403">All off</td>
      </tr>
      <tr>
+       <td></td>
     <td>Bestätigen</td>
     <td>
     <input type="submit" value="Submit">
     </td>
     </tr>
     </tr>
+      </form>
   </tbody> 
 </table>
 </div>
-<form formmethod="get" action="index.php">
-    <table>
-    <tr>
-      <th>Name</th>
-      <th>Farbe</th>
-     </tr>
-     <tr>
-    <td>LED-Band</td>
-    <td>
-      <input type="radio" name="relais1" value="100"> Red<br>
-      <input type="radio" name="relais1" value="404"> Green<br>
-      <input type="radio" name="relais1" value="101"> Blue<br>
-      <input type="radio" name="relais1" value="102"> Yellow<br>
-      <input type="radio" name="relais1" value="200"> Purple<br>
-      <input type="radio" name="relais1" value="203"> Cyan<br>
-      <input type="radio" name="relais1" value="206"> White<br>
-      <input type="radio" name="relais1" value="501"> Fan on<br>
-      <input type="radio" name="relais1" value="502"> Fan off<br>
-      <input type="radio" name="relais1" value="403"> All off<br>
-    </td>
-    </tr>
-    <tr>
-    <td>Bestätigen</td>
-    <td>
-    <input type="submit" value="Submit">
-    </td>
-    </tr>
-    </table>
-    </form>
         <p class="lead">
           <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
         </p>
