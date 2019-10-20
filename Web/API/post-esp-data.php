87c237ee73cd70fe2e06bb38ee1d80f4367ac7e8
Luca Lutz <iyfcjm@2mailcloud.com>
2019-10-20 10:45:50 +0200

    luca

--- Web/API/post-esp-data.php
+++ Web/API/post-esp-data.php
@@ -19,8 +19,8 @@ $data = array();
 foreach ($result as $row) {
   $data[] = $row;
 }
-echo $data["0"][relays1].$data["0"][relays2].$data["0"][relays3]."<br />"; //.$data["0"][relays4]
-$bin = $data["0"][relays1].$data["0"][relays2].$data["0"][relays3]; //.$data["0"][relays4]
+echo $data["0"][relays1]."<br />"; //.$data["0"][relays4]   .$data["0"][relays2].$data["0"][relays3]
+$bin = $data["0"][relays1]; //.$data["0"][relays4]    .$data["0"][relays2].$data["0"][relays3]
 http_response_code($bin);
 
 // Keep this API Key value to be compatible with the ESP32 code provided in the project page. 
