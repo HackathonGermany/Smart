91dc1729126dd5d747261e69a0c0a8cdf6ab8a63
Maris <maris.beer@icloud.com>
2019-10-20 13:03:53 +0200

    layout

--- Web/Login/index.php
+++ Web/Login/index.php
@@ -45,6 +45,7 @@ foreach ($result as $row) {
     <head>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">        <meta lang="deDE" charset="utf-8">
         <link rel="stylesheet" type="text/css" href="../css/style.css">
+        <link rel="stylesheet" href="../assets/css/index.css"
         <link rel="stylesheet" href="https://stsackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -68,7 +69,7 @@ foreach ($result as $row) {
         </style>
         <!-- <meta http-equiv="refresh" content="1; URL=/Login/"> -->
     </head>
-<body>
+<body class="text-center">
 <?php
 //echo "Hallo Benutzer mit der ID: ".$userid;
 ?>
