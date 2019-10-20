4ea8107fe883804307ffddafa680cca12f1f00bc
Maris <maris.beer@icloud.com>
2019-10-20 02:27:43 +0200

    local bootstrap

--- Web/Login/login.php
+++ Web/Login/login.php
@@ -30,7 +30,7 @@ if(isset($_GET['login'])) {
     <title>Login</title> 
 
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
     <link rel="icon" type="image/vnd.microsoft.icon" href="../assets/media/logo.ico">
 
     <!-- Custom styles for this template -->
